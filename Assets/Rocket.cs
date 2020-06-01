fabe5fd3b4af5aaacfa12e0e92b63961e7e364e4
Riley Worstell <rcwors02@gmail.com>
2020-05-31 23:38:57 -0400

    Added Sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+      rigidBody = GetComponent<Rigidbody>();
 
+      audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,13 +21,21 @@ public class Rocket : MonoBehaviour
     }
     private void ProcessInput() {
       if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
-        print("Thrusting");
+        //
+          rigidBody.AddRelativeForce(Vector3.up);
+          if (!audioSource.isPlaying) {
+            audioSource.play();
+          }
+
+      }
+      else {
+        audioSource.Stop();
       }
       if (Input.GetKey(KeyCode.A)) {
-        print("Rotating Left");
+        transform.Rotate(Vector3.forward);
       }
       else if (Input.GetKey(KeyCode.D)) {
-        print("Rotating Right");
+        transform.Rotate(-Vector3.forward);
       }
     }
 }
