b1a8a9ac828b8bd0aaaa266f621fd81aea2b42df
Riley Worstell <rcwors02@gmail.com>
2020-06-02 06:27:32 -0400

    Coroutine for death

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -11,6 +11,9 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,29 +24,41 @@ public class Rocket : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { if (state == State.Alive) {
+
+
       Thrust();
       Rotate();
     }
+    }
 
 void OnCollisionEnter(Collision collision) {
+  if (state != State.Alive) {
+    return;
+  }
   switch (collision.gameObject.tag) {
     case "Friendly":
       print("OK");
       break;
     case "Finish":
-    SceneManager.LoadScene(1);
-    print("Finish");
+    state = State.Transcending;
+    Invoke("LoadNextLevel", 1f);
+
     break;
     default:
-    print("dead");
-    SceneManager.LoadScene(0);
+    state = State.Dying;
+    Invoke("LoadFirstLevel", 1f);
     // destroy ship
         break;
     }
   }
 
-
+private void LoadNextLevel() {
+  SceneManager.LoadScene(1);
+}
+private void LoadFirstLevel() {
+  SceneManager.LoadScene(0);
+}
 
     private void Rotate() {
       rigidBody.freezeRotation = true; // take manual rotation
